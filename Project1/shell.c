b764d01415c15002a620e0349eb6f093d70b21c9
Ryan Attard <frontrunner4000@gmail.com>
2013-09-11 00:22:44 -0500

    Added comments to header for more status info

--- Project1/shell.c
+++ Project1/shell.c
@@ -9,19 +9,25 @@
 //5. Access executables in /bin and /usr/bin                       x
 //6. Execute executable without argument.                          x
 //7. Execute with output redirection to file.
+//      -define OUT,IN fds for processes depending on pipe/redirection combinations
 //8. Execute with output redirection to program.
 //9. Alarm after 5 seconds of execution.
+//   --sleep 5 in parent; send sigterm if prompted yes
 //10. Alarm prompts for termination. 
 //11. Alarm can be "OFF" in PROFILE(Time=-1)                       x
 //12. Alarm can be turned off via alarm off                        x
 //         alarm off
-//13. Alias expansion.                                             /
+//13. Alias expansion.                                             x
 //         Alias listcontent="ls -l | grep "^d""
 //14. Alias namespace duplication verification.                    x
 //         Alias listcontent="ls -l | grep "^d""
 //         Alias listcontent="ls -l | grep "^d""
 //15. Global namespace duplication verification for aliases.       ?
 //16. if/then/else/fi
+//    use /bin/test builtin;
+//    execp command between if/then redir stdout to /dev/null; 
+//    if child returns 0, == true, exec then
+//    if child returns 1, == false, exec else
 //17. Terminate only with exit                                     x
 //18. Handle signals where possible (CTRL+C, CTRL+Z)               x
 //19. Handle CTRL+D better
